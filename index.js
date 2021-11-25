9e7fa5575c22ff5356417579accf23b0d535dfec
Leah <elloworldt@gmail.com>
2021-11-25 08:35:35 -0800

    completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function introduction(name) {
+  console.log(`Hi, my name is ${name}.`);
+  }
+  introduction();
+
+function introductionWithLanguage (name,language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+ntroductionWithLanguage();
+
+function introductionWithLanguageOptional (name, language) {
+    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  }
+  introductionWithLanguageOptional();
