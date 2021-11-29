e509940d6781647f86113b04b40257872f52989a
Leah <elloworldt@gmail.com>
2021-11-28 18:04:23 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 
 function introduction(name) {
-  console.log('Hi, my name is'+ name);
+  return `Hi, my name is ${name}.`
   }
 
-  introduction(Josh);
-
-function introductionWithLanguage (name,language) {
-  console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+function introductionWithLanguage (name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
-
-ntroductionWithLanguage(Ember.js);
 
 function introductionWithLanguageOptional (name, language) {
-    console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
+  language = (typeof language !== 'undefined') ?  language : JavaScript
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
   }
-  
-  introductionWithLanguageOptional();
